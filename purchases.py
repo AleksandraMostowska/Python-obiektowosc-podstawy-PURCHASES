85190469f5d5ff86629bf4f9005e9ac13d1889f2
Aleksandra <ola.mostowska@op.pl>
2023-09-06 16:23:55 +0100

    Get data from file + validate

--- purchases.py
+++ purchases.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
 from decimal import Decimal
+import re
+from re import Match
+from typing import Self
 
 
 # The Customer class features data of given customer, including name, last name, age and cash.
@@ -18,12 +21,51 @@ class Product:
     category: str
     price: Decimal
 
-def main() -> None:
-    customer = Customer('AAA', 'BBBBB', 20, 2000)
-    print(customer)
 
-    product = Product('XBOX', 'RTV AGD', '3500')
-    print(product)
+# The Validator class checks whether the given line matches the pattern.
+class Validator:
+    @staticmethod
+    def check_if_correct(line: str, pattern: str) -> Match[str] | None:
+        return re.match(pattern, line)
+
+
+# The Purchases class holds information about customers and their purchases.
+@dataclass
+class Purchases:
+    customers_and_their_products: dict[Customer, dict[Product, int]]
+
+    # The method gets data from file and checks via Validator whether text in the file has correct form.
+    @classmethod
+    def get_data(cls, file_name: str) -> Self:
+        purchases = {}
+        with open(file_name, 'r') as f:
+            for line in f.readlines():
+                if not Validator.check_if_correct(line, r'^([A-Z][a-z]+;){2}\d+;\d+ \[(([A-Z][a-z]+;){2}\d+)( ([A-Z][a-z]+;){2}\d+)*\]\n?$'):
+                    raise ValueError('Input not correct')
+
+                customer_data, product_data = line.strip().split(' [')
+                customer_tmp = customer_data.split(';')
+                customer = Customer(customer_tmp[0], customer_tmp[1], int(customer_tmp[2]), Decimal(customer_tmp[3]))
+
+                products_info = product_data[:-1].split(' ')
+                for p in products_info:
+                    p_tmp = p.split(';')
+                    product = Product(p_tmp[0], p_tmp[1], Decimal(p_tmp[2]))
+
+                    if customer not in purchases:
+                        purchases[customer] = {}
+
+                    if product not in purchases[customer]:
+                        purchases[customer][product] = 1
+                    else:
+                        purchases[customer][product] += 1
+
+        return Purchases(purchases)
+
+
+def main() -> None:
+    data = Purchases.get_data('customer_and_products.txt')
+    print(data)
 
 if __name__ == '__main__':
     main()
