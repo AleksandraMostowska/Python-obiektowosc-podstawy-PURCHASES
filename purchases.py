e4ed98b8e6987b569c1728fbdf2c0f2624b9f3c2
Aleksandra <ola.mostowska@op.pl>
2023-09-06 18:20:30 +0100

    Methods - grouping 2

--- purchases.py
+++ purchases.py
@@ -1,9 +1,10 @@
+from _decimal import Decimal
 from dataclasses import dataclass
 from decimal import Decimal
 import re
 from re import Match
-from typing import Self, Any
-from collections import defaultdict
+from typing import Self, Any, Dict
+from collections import defaultdict, Counter
 
 
 # The Customer class features data of given customer, including name, last name, age and cash.
@@ -109,6 +110,72 @@ class Purchases:
 
         return age_and_category
 
+    # The method gets the average price of product in categories.
+    def get_average_price_and_category(self) -> dict[str, Decimal]:
+        category_and_price = defaultdict(list)
+        for customer, products in self.customers_and_their_products.items():
+            for product in products:
+                category_and_price[product.category].append(product.price)
+
+        return dict([(category, sum(prices) / len(prices)) for category, prices in category_and_price.items()])
+
+    # The method returns most and least expensive products in category.
+    def get_most_and_least_expensive_in_category(self) -> defaultdict[Any, list]:
+        category_and_prices = defaultdict(list)
+        for customer, products in self.customers_and_their_products.items():
+            for product in products:
+                category_and_prices[product.category].append(product)
+
+        for cat, pr in category_and_prices.items():
+            category_and_prices[cat] = [max(pr, key=lambda x: x.price), min(pr, key=lambda x: x.price)]
+
+        return category_and_prices
+
+    # The method returns customers and the category from which they bought the most products.
+    def get_most_frequent_category_for_customers(self) -> dict[str, list[Customer]]:
+        customer_and_products = defaultdict(list)
+        for customer, products in self.customers_and_their_products.items():
+            for product, quantity in products.items():
+                customer_and_products[customer].extend([product for _ in range(quantity)])
+
+        customer_with_product_pairs = CustomerWithProduct.create_pairs(customer_and_products)
+
+        result_pairs = defaultdict(list)
+        for item in customer_with_product_pairs:
+            result_pairs[item.product.category].append(item.customer)
+
+        for category, customers in result_pairs.items():
+            result_pairs[category] = Counter(customers).most_common(1)[0][0]
+
+        return result_pairs
+
+    # The method checks whether the customer has enough cash to pay for the purchases.
+    def can_customer_pay(self) -> dict[Customer, Decimal]:
+        return dict([(customer, customer.cash - self.get_total_customer_spent(customer)) for customer, products in self.customers_and_their_products.items()])
+
+
+# The CustomerWithProduct class creates pairs (customer, product) in order to help in method
+# get_most_frequent_category_for_customers, so that later I can count how many times customers bought a product
+# from this category and get the max from it.
+@dataclass
+class CustomerWithProduct:
+    customer: Customer
+    product: Product
+
+    # This method converts given customer and product to class object.
+    @classmethod
+    def convert_to_customer_with_product(cls, c: Customer, p: Product) -> Self:
+        return cls(c, p)
+
+    # The method creates pairs (customer, product).
+    @staticmethod
+    def create_pairs(pairs: dict[Customer, list[Product]]) -> list[Self]:
+        result = []
+        for c, products in pairs.items():
+            result.extend([CustomerWithProduct.convert_to_customer_with_product(c, p) for p in products])
+
+        return result
+
 
 def main() -> None:
     data = Purchases.get_data('customer_and_products.txt')
@@ -120,6 +187,14 @@ def main() -> None:
     print(f'Customers who spent the most in category: {data.get_most_spending_in_category(category)}')
 
     print(f'Age and category: {data.get_age_and_category()}')
+
+    print(f'Average price in category: {data.get_average_price_and_category()}')
+
+    print(f'Most and least expensive products: {data.get_most_and_least_expensive_in_category()}')
+
+    print(f'Customers and their most frequent category: {data.get_most_frequent_category_for_customers()}')
+
+    print(f'Can customer pay? {data.can_customer_pay()}')
 
 if __name__ == '__main__':
     main()
